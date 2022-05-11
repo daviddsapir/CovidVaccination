b0f07cf32dad9a7867fa40bb1a8b025c6d2d2dae
David Sapir <daviddsapir@gmail.com>
2022-05-11 22:44:06 +0300

    Added route

--- front_end/src/Components/Home/HomePage.jsx
+++ front_end/src/Components/Home/HomePage.jsx
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
 export default function HomePage() {
+    const navigate = useNavigate();
+
     return (
         <>
-            <header className="my-5">
+            <header className="mt-5">
                 <div className="container">
                     <div className="row justify-content-center">
                         <div className="col-6">
@@ -19,7 +23,7 @@ export default function HomePage() {
                                 <div className="card-body">
                                     <h5 className="card-title fw-bold">About Us</h5>
                                     <p className="card-text lead">
-                                        we help undeveloped countries vaccinate their people! <br />
+                                        We help undeveloped countries vaccinate their people! <br />
                                         Some countries do not keep electronic records of their <br />
                                         citizens medical situation. We want to help them collect <br />
                                         data from citizens in order to prioritise COVID-19 vaccination.
@@ -28,6 +32,45 @@ export default function HomePage() {
                             </div>
                         </div>
                     </div>
+
+                    <div className="row justify-content-center my-4">
+                        <div className="col-6">
+                            <div className="card border border-5 border">
+                                <div className="card-body">
+                                    <h5 className="card-title fw-bold">Register Page</h5>
+                                    <p className="card-text lead">
+                                        Explaining of the study journey including my previous knowledge and what are the resources (YouTube,
+                                        Tutorials, installations,…) i've used for learning.
+                                    </p>
+                                    <div className="text-center">
+                                        <button className="btn btn-primary border border-5 border-info"
+                                                onClick={() => {
+                                            navigate("/register");
+                                        }}>
+                                            Go To Register Page
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row justify-content-center my-4">
+                        <div className="col-6">
+                            <div className="card border border-5">
+                                <div className="card-body">
+                                    <h5 className="card-title fw-bold">Summary Page</h5>
+                                    <p className="card-text lead">A tabular view that shows all the existing registration info available</p>
+                                    <div className="text-center">
+                                        <a href="#" className="btn btn-primary border border-5 border-info">
+                                            Go To Summary Page
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                     <div className="row justify-content-center my-4">
                         <div className="col-6">
                             <div className="card border border-5 border">
@@ -40,21 +83,6 @@ export default function HomePage() {
                                     <div className="text-center">
                                         <a href="#" className="btn btn-primary border border-5 border-info">
                                             Go To Study Journey
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row justify-content-center my-4">
-                        <div className="col-6">
-                            <div className="card border border-5">
-                                <div className="card-body">
-                                    <h5 className="card-title fw-bold">Summary Page</h5>
-                                    <p className="card-text lead">A tabular view that shows all the existing registration info available</p>
-                                    <div className="text-center">
-                                        <a href="#" className="btn btn-primary border border-5 border-info">
-                                            Go To Summary Page
                                         </a>
                                     </div>
                                 </div>
--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,17 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import RegisterPage from "./Components/Registration/RegisterPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import HomePage from "./Components/Home/HomePage";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <HomePage></HomePage>
-        {/* <RegisterPage></RegisterPage> */}
+        <Router>
+            <Routes>
+                <Route path="" element={<HomePage />}/>
+                <Route path="/register" element={<RegisterPage />}/>
+            </Routes>
+        </Router>
     </React.StrictMode>
 );
 
