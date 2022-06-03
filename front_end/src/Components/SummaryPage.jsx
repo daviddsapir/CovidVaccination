4cb266160155e7b34e81bcf1b4c0ec944c0ff093
David Sapir <daviddsapir@gmail.com>
2022-06-03 12:03:30 +0300

    updated form filter

--- front_end/src/Components/SummaryPage.jsx
+++ front_end/src/Components/SummaryPage.jsx
@@ -2,9 +2,36 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function SummaryPage() {
-	const [formData, setFormData] = useState([]);
+	const summaryPageFromInfoUrl = "SummaryPage/getFormInfo";
 
-	const [filterByCity, setFilterByCity] = useState(null);
+	const [formData, setFormData] = useState([
+		{
+			"first_name": "asd",
+			"last_name": "123",
+			"date_of_birth": "0123-03-12",
+			"address": "123",
+			"city": "One",
+			"zip_code": "123123",
+			"land_line": "123",
+			"cellular_phone": "123",
+			"been_infected": false,
+			"other_conditions": "Cardio_Vascular, cancer, obesity, MRSA"
+		},
+		{
+			"first_name": "asd",
+			"last_name": "asd",
+			"date_of_birth": "2323-02-23",
+			"address": "123",
+			"city": "One",
+			"zip_code": "123",
+			"land_line": "123",
+			"cellular_phone": "123",
+			"been_infected": true,
+			"other_conditions": "diabetes, cancer, MRSA, cholesterol"
+		}
+	]);
+
+	const [filterByCity, setFilterByCity] = useState("none");
 
 	const [filterByDate, setFilterByDate] = useState({
 		beg: null,
@@ -13,7 +40,7 @@ export default function SummaryPage() {
 
 	useEffect(() => {
 		axios
-			.get("SummaryPage/getFormInfo")
+			.get(summaryPageFromInfoUrl)
 			.then((response) => {
 				const data = response.data.data;
 				setFormData(data);
@@ -25,17 +52,21 @@ export default function SummaryPage() {
 	function isFilterPassed(data) {
 		const [dateOfBirth, city] = [data.date_of_birth, data.city];
 
-		if (filterByCity === "" && filterByDate.min === null && filterByDate.max === null) {
+		if (filterByCity === "none" && filterByDate.beg === null && filterByDate.end === null) {
 			return true;
 		}
 
-		if (filterByCity !== "" && filterByCity !== city) {
+		if (filterByCity !== "none") {
+			if (filterByCity)
+			console.log(filterByCity, city)
 			return false;
 		}
 
-		if (filterByCity !== null) {
+		if (filterByDate.beg !== null && filterByDate.end !== null) {
+			console.log(filterByDate)
 			const currentDate = new Date(dateOfBirth);
-			if (currentDate >= filterByDate.min && currentDate <= filterByDate.max) {
+			console.log(currentDate);
+			if (currentDate >= filterByDate.beg && currentDate <= filterByDate.end) {
 				return true;
 			}
 		}
@@ -49,34 +80,56 @@ export default function SummaryPage() {
 				<div className="row m-5 justify-content-center">
 					<div className="col-4 mx-5">
 						<div className="card">
-							<div className="card-header lead fw-bold bg-light">
-								Filter By City
-							</div>
+							<div className="card-header lead fw-bold bg-light">Filter By City</div>
 							<div className="card-body bg-light">
-								<select className="form-select" size="3" aria-label="size 3 select example">
-									<option value="1">One</option>
-									<option value="2">Two</option>
-									<option value="3">Three</option>
+								<select
+									className="form-select"
+									aria-label="Default select example"
+									onChange={(event) => {
+										const city = event.target.value;
+										setFilterByCity(city);
+									}}
+								>
+									<option value="none">none</option>
+									<option value="One">One</option>
+									<option value="Two">Two</option>
+									<option value="Three">Three</option>
 								</select>
 							</div>
 						</div>
 					</div>
 					<div className="col-4">
 						<div className="card">
-							<div className="card-header lead fw-bold bg-light">
-								Filter By Birth Date
-							</div>
+							<div className="card-header lead fw-bold bg-light">Filter By Birth Date</div>
 							<div className="card-body bg-light">
 								<div className="row g-3">
 									<div className="col">
-										<label className="lead" htmlFor="">From</label>
-										<input type="date" className="form-control" placeholder="Begin Date"
-											   aria-label="beg date" />
+										<label className="lead">From</label>
+										<input
+											type="date"
+											className="form-control"
+											placeholder="Begin Date"
+											aria-label="beg date"
+											onChange={(event) => {
+												let begDate = filterByDate;
+												begDate.beg = event.target.value;
+												setFilterByDate(begDate);
+											}}
+										/>
 									</div>
 									<div className="col">
-										<label className="lead" htmlFor="">To</label>
-										<input type="date" className="form-control" placeholder="End Date"
-											   aria-label="end date" />
+										<label className="lead">To</label>
+										<input
+											type="date"
+											className="form-control"
+											placeholder="End Date"
+											aria-label="end date"
+											onChange={(event) => {
+												let endDate = filterByDate;
+												endDate.end = event.target.value;
+												setFilterByDate(endDate);
+											}}
+										/>
 									</div>
 								</div>
 							</div>
@@ -88,26 +141,46 @@ export default function SummaryPage() {
 	}
 
 	function renderTableRows() {
-		return formData.map((data) => {
-			// if (isFilterPassed(data)) {
+		const filteredData = formData.filter((data) => {
+			if (isFilterPassed(data)) {
+				return true;
+			}
+			console.log("here");
+			return false;
+		});
+
+		if (filteredData.length > 0) {
+			return formData.map((data, index) => {
+				return (
+					<>
+						<tr>
+							<td>{data.first_name}</td>
+							<td>{data.last_name}</td>
+							<td>{data.date_of_birth}</td>
+							<td>{data.address}</td>
+							<td>{data.city}</td>
+							<td>{data.zip_code}</td>
+							<td>{data.land_line}</td>
+							<td>{data.cellular_phone}</td>
+							<td>{data.been_infected ? "Been Infected" : ""}</td>
+							<td>{data.other_conditions}</td>
+						</tr>
+					</>
+				);
+			});
+		} else {
 			return (
 				<>
-					<tr>
-						<td>{data.first_name}</td>
-						<td>{data.last_name}</td>
-						<td>{data.date_of_birth}</td>
-						<td>{data.address}</td>
-						<td>{data.city}</td>
-						<td>{data.zip_code}</td>
-						<td>{data.land_line}</td>
-						<td>{data.cellular_phone}</td>
-						<td>{data.been_infected ? "Been Infected" : ""}</td>
-						<td>{data.other_conditions}</td>
-					</tr>
+					<div className="container">
+						<div className="row justify-content-center">
+							<div className="col">
+								<h2 className="text-center">No Results</h2>
+							</div>
+						</div>
+					</div>
 				</>
 			);
-			// }
-		});
+		}
 	}
 
 	function renderFormResults() {
@@ -160,9 +233,7 @@ export default function SummaryPage() {
 				</div>
 			</header>
 			<main>
-				<div className="container">
-					{renderFilterForm()}
-				</div>
+				<div className="container">{renderFilterForm()}</div>
 				<div className="container-fluid">{renderFormResults()}</div>
 			</main>
 		</>
